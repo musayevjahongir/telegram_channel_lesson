b725a90ddbb4947f2870cfc3057e6e1a18e7a229
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-09 20:29:06 +0500

    hamma oyda qanchadan xabar

--- get_post_per_month.py
+++ get_post_per_month.py
@@ -11,4 +11,11 @@ def get_post_per_month(data:dict)->dict:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    d={}
+    for i in data["messages"]:
+        d[int(i["date"][5:7])]=0
+    for i in data["messages"]:
+        d[int(i["date"][5:7])]+=1
+    return d
+data=fromJson("data/result.json")
+print(get_post_per_month(data))
