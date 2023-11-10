63d1fff624ce479324df3fc756c46ccf8959e769
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-10 09:05:07 +0500

    hafta kunlarida qancha habar ekanini aniqlash

--- get_post_weekday.py
+++ get_post_weekday.py
@@ -1,9 +1,19 @@
 from read_data import fromJson
-def get_post_weekday(data:dict,week:int)->dict:
+import datetime
+def get_post_weekday(data:dict)->dict:
     """
     Return the number of posts for each weekday
     args:
         data: a dictionary of posts
     returns: a dictionary with the number of posts for each weekday
     """
-    return
+    d={}
+    for i in data["messages"]:
+        s=datetime.datetime.fromisoformat(i["date"])
+        d[s.isoweekday()]=0
+    for i in data["messages"]:
+        s=datetime.datetime.fromisoformat(i["date"])
+        d[s.isoweekday()]+=1
+    return d
+data=fromJson("data/result.json")
+print(get_post_weekday(data, ))
